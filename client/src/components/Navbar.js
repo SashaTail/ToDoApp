118c80a03a288afefae9089689a89dc9f92f71ae
Alex <tail2013s@gmail.com>
2021-08-30 16:31:52 +1000

    update navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
+import './header.css';
 import React, { useContext } from 'react'
 import {useHistory} from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
-
 export const Navbar=  () =>
 {
     const history = useHistory()
@@ -13,31 +13,22 @@ export const Navbar=  () =>
 
     }
     return ( 
-        <nav className="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
-        <div className="container">
-            <div className="navbar-brand">
-                <span className="navbar-logo">
-                </span>
-                <span className="navbar-caption-wrap" ><a style={{color:'black', fontFamily:'Segoe UI', fontWeight:"500"}} href="/">Список<br/>твоих<br/>дел</a></span>
+        <div className='container_nav'>
+                <div className="logo_nav">
+                <a className="logo_text_" style={{textDecoration: 'none'}} href="/">Список<br/>твоих<br/>дел</a>
             </div>
-            <div className="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
-                <ul className="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
-                <li className="nav-item" ><a class="nav-link link text-black" href="/" >Menu Item 1</a></li>
-                {auth.isAuthenticated ?
-                        (<li className="nav-item" style={{paddingRight: '5rem'}}>
-                        
-                        <a className="nav-link link text-black" href="/" onClick={logoutHandler}>Выйти</a>
-                        </li>)
-                        : (<li className="nav-item" style={{paddingRight: '5rem'}}>
-                        
-                        <a className="nav-link link text-black" href="/auth">Авторизация</a>
-                        </li>)}
-                        </ul>
-                
-                
+            <ul>
+            <div className="navigation_">
+                    <li className="navigation_item_">
+                        <a className="navigation_text_" style={{textDecoration: 'none'}} href='/'>Menu Item1</a>
+                    </li>
+                    {auth.isAuthenticated ?
+                        (<li className="navigation_item_"><a  className= "navigation_text_" style={{textDecoration: 'none'}} href='/' onClick={logoutHandler}>Выйти</a></li>) :
+                        (<li className="navigation_item_"><a  className= "navigation_text_" style={{textDecoration: 'none'}} href='/auth'>Авторизация</a></li>)}
             </div>
+            </ul>
+            
         </div>
-    </nav>
 
     )
 }
